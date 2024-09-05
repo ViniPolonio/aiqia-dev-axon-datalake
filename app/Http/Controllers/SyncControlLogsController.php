b2cd3431d46c3ae6eb19efcfaeea56f9674eda15
Vinicius Polonio <125396776+ViniPolonio@users.noreply.github.com>
2024-09-05 11:42:53 -0300

    alteração os status de retorno.

--- app/Http/Controllers/SyncControlLogsController.php
+++ app/Http/Controllers/SyncControlLogsController.php
@@ -48,7 +48,7 @@ class SyncControlLogsController extends Controller
             return response()->json([
                 'data' => $results
             ], 200);
-        } 
+        }
         catch (\Exception $e) {
             return response()->json([
                 'error' => $e->getMessage()
@@ -57,119 +57,103 @@ class SyncControlLogsController extends Controller
     }
 
     public function show($id, Request $request) 
-{
-    try {
-        // Crie uma instância do SyncControlConfigController
-        $syncControlConfigController = new SyncControlConfigController();
+    {
+        try {
+            $syncControlConfigController = new SyncControlConfigController();
 
-        // Chame o método consultTimer passando o id em forma de array
-        $consultSyncControlConfig = $syncControlConfigController->consultTimer([$id]);
+            $consultSyncControlConfig = $syncControlConfigController->consultTimer([$id]);
 
-        // Verifique se o consultSyncControlConfig retornou status 0
-        if (isset($consultSyncControlConfig['status']) && $consultSyncControlConfig['status'] == 0) {
-            return response()->json([
-                'status' => 0,
-                'message' => $consultSyncControlConfig['message']
-            ], 404);
-        }
-
-        // Verifique se a configuração de timer está presente
-        $configTime = collect($consultSyncControlConfig)->firstWhere('sync_control_config_id', $id);
-
-        // Obtenha a configuração
-        $config = SyncControlConfig::find($id);
-
-        if (!$config) {
-            return response()->json([
-                'status' => 0,
-                'message' => 'SyncControlConfig not found.'
-            ], 404);
-        }
-
-        // Adicione o interval_description e interval_status ao config
-        $config->interval_description = $configTime['interval_description'] ?? 0;
-        $config->interval_status = $configTime ? '' : 0; 
-
-        // Se a configuração de timer existir, verifique se está ativa
-        if ($configTime) {
-            $intervalType = $configTime['interval_type'];
-            $intervalValue = $configTime['interval_value'];
-            $intervalDescription = $configTime['interval_description'];
-
-            $intervalInMinutes = match ($intervalType) {
-                1 => $intervalValue,            
-                2 => $intervalValue * 60,       
-                3 => $intervalValue * 1440,     
-                default => null,                
-            };
-
-            // Obtenha o último log para a configuração
-            $lastLog = SyncControlLog::where('sync_control_config_id', $id)
-                ->orderBy('finished_at', 'desc')
-                ->select(['finished_at'])
-                ->first();
-
-            $inactiveMessage = '';
-
-            // Verifique se o último log foi encontrado e se o tempo está dentro do intervalo
-            if ($intervalInMinutes && $lastLog && $lastLog->finished_at) {
-                $timeDifference = \Carbon\Carbon::now()->diffInMinutes(\Carbon\Carbon::parse($lastLog->finished_at));
-
-                if ($timeDifference > $intervalInMinutes) {
-                    $inactiveMessage = 'Configuration is inactive due to time interval exceeded.';
-                }
+            if (isset($consultSyncControlConfig['status']) && $consultSyncControlConfig['status'] == 0) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => $consultSyncControlConfig['message']
+                ], 404);
             }
 
-            // Adicione o intervalo_status baseado na verificação acima
-            $config->interval_status = $inactiveMessage ?: 1; // Define como 'Active' se $inactiveMessage estiver vazio
-        }
+            $configTime = collect($consultSyncControlConfig)->firstWhere('sync_control_config_id', $id);
 
-        // Obtenha os logs com paginação
-        $cursor = $request->input('cursor'); 
-        $perPage = 20; 
+            $config = SyncControlConfig::find($id);
 
-        if ($cursor !== null && !is_string($cursor)) {
+            if (!$config) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'SyncControlConfig not found.'
+                ], 404);
+            }
+
+            $config->interval_description = $configTime['interval_description'] ?? 0;
+            $config->interval_status = $configTime ? '' : 0; 
+
+            if ($configTime) {
+                $intervalType = $configTime['interval_type'];
+                $intervalValue = $configTime['interval_value'];
+                $intervalDescription = $configTime['interval_description'];
+
+                $intervalInMinutes = match ($intervalType) {
+                    1 => $intervalValue,            
+                    2 => $intervalValue * 60,       
+                    3 => $intervalValue * 1440,     
+                    default => null,                
+                };
+
+                $lastLog = SyncControlLog::where('sync_control_config_id', $id)
+                    ->orderBy('finished_at', 'desc')
+                    ->select(['finished_at'])
+                    ->first();
+
+                $inactiveMessage = '';
+
+                if ($intervalInMinutes && $lastLog && $lastLog->finished_at) {
+                    $timeDifference = \Carbon\Carbon::now()->diffInMinutes(\Carbon\Carbon::parse($lastLog->finished_at));
+
+                    if ($timeDifference > $intervalInMinutes) {
+                        $inactiveMessage = 2;
+                    }
+                }
+
+                $config->interval_status = $inactiveMessage ?: 1; 
+            }
+
+            $cursor = $request->input('cursor'); 
+            $perPage = 20; 
+
+            if ($cursor !== null && !is_string($cursor)) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'Invalid cursor format.'
+                ], 400);
+            }
+
+            $logs = SyncControlLog::where('sync_control_config_id', $id)
+                ->orderBy('finished_at', 'desc')
+                ->cursorPaginate($perPage, ['*'], 'cursor', $cursor);
+
+            if ($logs->isEmpty()) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'No logs found for this SyncControlConfig.',
+                    'data' => [
+                        'config' => $config,
+                    ]
+                ], 404);
+            }
+
             return response()->json([
-                'status' => 0,
-                'message' => 'Invalid cursor format.'
-            ], 400);
-        }
-
-        $logs = SyncControlLog::where('sync_control_config_id', $id)
-            ->orderBy('finished_at', 'desc')
-            ->cursorPaginate($perPage, ['*'], 'cursor', $cursor);
-
-        if ($logs->isEmpty()) {
-            return response()->json([
-                'status' => 0,
-                'message' => 'No logs found for this SyncControlConfig.',
+                'status' => 1,
                 'data' => [
                     'config' => $config,
+                    'logs' => $logs,
+                    'has_more' => $logs->hasMorePages()
                 ]
-            ], 404);
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 0,
+                'message' => 'An error has occurred: ' . $e->getMessage()
+            ], 500);
         }
-
-        return response()->json([
-            'status' => $inactiveMessage ? 0 : 1,
-            'message' => $inactiveMessage ?: 'Success',
-            'data' => [
-                'config' => $config,
-                'logs' => $logs,
-                'has_more' => $logs->hasMorePages()
-            ]
-        ], 200);
-
-    } catch (\Exception $e) {
-        return response()->json([
-            'status' => 0,
-            'message' => 'An error has occurred: ' . $e->getMessage()
-        ], 500);
     }
-}
-
-
-
-
 
     public function returnShowTableConfig($id) {
         try {
